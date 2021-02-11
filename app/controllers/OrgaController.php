a09e4f24441dad23cf5f24086a85b96167aea15b
Simon <sim.debray@laposte.net>
2021-02-11 22:01:09 +0100

    add : Create crud

--- app/controllers/OrgaController.php
+++ app/controllers/OrgaController.php
@@ -1,9 +1,12 @@
 <?php
 namespace controllers;
  use models\Organization;
+ use Ubiquity\attributes\items\router\Get;
+ use Ubiquity\attributes\items\router\Post;
  use Ubiquity\attributes\items\router\Route;
  use Ubiquity\orm\DAO;
  use Ubiquity\orm\repositories\ViewRepository;
+ use Ubiquity\utils\http\URequest;
 
  /**
   * Controller OrgaController
@@ -29,5 +32,25 @@ class OrgaController extends ControllerBase{
         //$orga =DAO::getById(Organization::class,$idOrga,['users.groupes','groupes.users']);
 		$this->loadDefaultView();
 	}
+
+    #[Route(path : 'orga/add', name : "orga.addOrgaForm")]
+    public function addOrgaForm(){
+        if(URequest::filled('valid')){
+            if (URequest::filled('name') && URequest::filled('domain')){
+                $orga=new Organization();
+                $orga->setName(URequest::post('name'));
+                $orga->setDomain(URequest::post('domain'));
+                $orga->setAliases(URequest::post('alias'));
+                URequest::setValuesToObject($orga);
+                if(DAO::insert($orga)){
+                    echo 'Nouvelle organisation insérée';
+                }
+            }else{
+                echo 'Remplir les champs';
+            }
+        }
+        print_r($_POST);
+        $this->loadView("OrgaController/orgaForm.html");
+    }
 
 }
